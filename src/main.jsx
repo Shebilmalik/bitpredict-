14b1921fe66f20f95d49638ceba53ad17a29488c
Shebilmalik <fadilmuhammed3602@gmail.com>
2026-03-11 00:52:11 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { WalletProvider } from "@btc-vision/walletconnect";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <WalletProvider>
-      <App />
-    </WalletProvider>
-  </StrictMode>
+  <StrictMode><App /></StrictMode>
 );
